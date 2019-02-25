88895bce3c8ea16948e80eb763b4ea67205c77c0
lmarioni <lucasmarionilm@gmail.com>
2019-02-24 23:11:01 -0300

    Initial commit

---
+++ build/precache-manifest.198e019549a96f73f4979fbcb55b4234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4149a754ca13f62bd853",
+    "url": "/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "4149a754ca13f62bd853",
+    "url": "/static/js/main.4149a754.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4142c9118043a638a5a4",
+    "url": "/static/css/2.a6052680.chunk.css"
+  },
+  {
+    "revision": "4142c9118043a638a5a4",
+    "url": "/static/js/2.4142c911.chunk.js"
+  },
+  {
+    "revision": "a4c0cb7f82a348cd477adcd643e6b80d",
+    "url": "/index.html"
+  }
+];
